177b4c3deb609605b1252c72a3bb6d8bf6184304
Ckyever Gaviola <ckyeverg@gmail.com>
2025-12-03 11:17:16 +1100

    Add script to clean database

---
+++ database/clean.js
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+
+import "dotenv/config";
+import { Client } from "pg";
+
+const SQL = `
+DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS users;
+`;
+
+async function main() {
+  console.log("Cleaning database...");
+  const client = new Client({
+    connectionString: process.env.DATABASE_CONNECTION_STRING,
+  });
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+  console.log("Done");
+}
+
+main();
