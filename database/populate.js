b29bd6f7572acc3d7666b3ffdd8187093edb691e
Ckyever Gaviola <ckyeverg@gmail.com>
2025-12-05 14:32:31 +1100

    Add unique index to username column

--- database/populate.js
+++ database/populate.js
@@ -6,7 +6,7 @@ import { Client } from "pg";
 const SQL = `
 CREATE TABLE IF NOT EXISTS users (
   id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  username VARCHAR(255) NOT NULL,
+  username VARCHAR(255) UNIQUE NOT NULL,
   password VARCHAR(255) NOT NULL,
   firstname VARCHAR(50),
   lastname VARCHAR(50),
