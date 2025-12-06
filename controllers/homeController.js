387499e78e4fdb8c82f8f169dd438065f12368c3
Ckyever Gaviola <ckyeverg@gmail.com>
2025-12-06 16:42:29 +1100

    Allow logged out users to view the home page

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,15 +2,11 @@ import { getPosts } from "../database/queries.js";
 
 const getHomePage = async (req, res) => {
   const posts = await getPosts();
-  if (res.locals.currentUser) {
-    res.render("index", {
-      title: "Clubhaus",
-      page: "pages/home",
-      posts: posts,
-    });
-  } else {
-    res.redirect("/login");
-  }
+  res.render("index", {
+    title: "Clubhaus",
+    page: "pages/home",
+    posts: posts,
+  });
 };
 
 export { getHomePage };
